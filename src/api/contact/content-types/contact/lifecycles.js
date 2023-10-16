151f2b56a6a5fafc0480f6ca86e9f13ae22812b3
Prawee Wongsa <prawee@hotmail.com>
2023-10-16 09:43:59 +0700

    decode mobile data with find one

--- src/api/contact/content-types/contact/lifecycles.js
+++ src/api/contact/content-types/contact/lifecycles.js
@@ -1,7 +1,14 @@
 module.exports = {
     async beforeCreate(event) {
-        console.log('beforeSave ', event)
+        // console.log('beforeSave ', event)
         // event.params.data.mobile = 'xyzaaaaaaa'
         event.params.data.mobile = btoa(event.params.data.mobile)
+    },
+    async afterFindOne(event) {
+        // console.log('afterFineOne', event.result.mobile);
+        event.result.mobile = atob(event.result.mobile)
+    },
+    async afterFindMany(event) {
+        console.log('afterFindMany', event.result[5]);
     }
 }
